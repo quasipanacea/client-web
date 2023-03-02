f1e0242c75d208ced0d249690e1dab5dd1f8253a
Edwin Kofler <edwin@kofler.dev>
2023-03-01 19:43:17 -0800

    install dependencies for rendering markdown to html

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const defaultOverview = 'by-collection'
+const defaultOverview = 'graph-cytoscape'
 
 export default createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
