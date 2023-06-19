78cac2cbc6d0d784001ec4333098a63979e3f8ac
Edwin Kofler <edwin@kofler.dev>
2023-06-19 02:51:50 -0700

    Improve plugin management

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import '@client/assets/base.css'
 
 import App from '@client/App.vue'
 import router from '@client/router/index.ts'
+import { initPlugins } from '@client/util/util.ts'
+
+await initPlugins()
 
 const app = createApp(App)
 app.use(createPinia())
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,46 +1,51 @@
-import {
-	overviewPlugins,
-	podPlugins,
-	modelPlugins,
-	viewPlugins,
-} from '@quasipanacea/pack-core/_client.js'
+import { getPlugins } from "@quasipanacea/common/client/plugin.ts"
+
+import { initAll } from '@quasipanacea/pack-core/_client.js'
+
+export async function initPlugins() {
+	await initAll()
+}
 
 export async function importOverview(pluginId: string): Promise<unknown> {
-	for (const overviewPlugin of overviewPlugins) {
-		if (overviewPlugin.metadata.id === pluginId) {
-			return overviewPlugin.component
-		}
+	const { overviewPlugins } = await getPlugins()
+
+	const plugin = overviewPlugins.get(pluginId)
+	if (!plugin) {
+		throw new Error(`Failed to find overview plugin with id: ${pluginId}`)
 	}
 
-	throw new Error(`Failed to find overview with id: ${pluginId}`)
+	return plugin.component
 }
 
 export async function importPod(pluginId: string): Promise<unknown> {
-	for (const podPlugin of podPlugins) {
-		if (podPlugin.metadata.id === pluginId) {
-			return podPlugin.component
-		}
+	const { podPlugins } = await getPlugins()
+
+	const plugin = podPlugins.get(pluginId)
+	if (!plugin) {
+		throw new Error(`Failed to find pod plugin with id: ${pluginId}`)
 	}
 
-	throw new Error(`Failed to find pod with id: ${pluginId}`)
+	return plugin.component
 }
 
 export async function importModel(pluginId: string): Promise<unknown> {
-	for (const modelPlugin of modelPlugins) {
-		if (modelPlugin.metadata.id === pluginId) {
-			return modelPlugin.component
-		}
+	const { modelPlugins } = await getPlugins()
+
+	const plugin = modelPlugins.get(pluginId)
+	if (!plugin) {
+		throw new Error(`Failed to find model plugin with id: ${pluginId}`)
 	}
 
-	throw new Error(`Failed to find model with id: ${pluginId}`)
+	return plugin.component
 }
 
 export async function importView(pluginId: string): Promise<unknown> {
-	for (const viewPlugin of viewPlugins) {
-		if (viewPlugin.metadata.id === pluginId) {
-			return viewPlugin.component
-		}
+	const { viewPlugins } = await getPlugins()
+
+	const plugin = viewPlugins.get(pluginId)
+	if (!plugin) {
+		throw new Error(`Failed to find view plugin with id: ${pluginId}`)
 	}
 
-	throw new Error(`Failed to find view with id: ${pluginId}`)
+	return plugin.component
 }
